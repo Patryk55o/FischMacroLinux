e1a0429506cd848def8e4ec2c82e3f919477fcf1
Patryk55o <186469709+Patryk55o@users.noreply.github.com>
2026-08-25 21:32:46 +0200

    New GUI stuff

---
+++ FischTuxGUI.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello! You ran the GUI Program in your Terminal, this is used to debug the macro. You can use this to see some useful stuff."
+source $PWD/bin/activate
+python3 $PWD/etc/gui.py
